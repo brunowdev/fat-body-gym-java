b8c7f03afe64cfc734041dd8595ac11c89e49f8a
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2016-11-27 00:32:53 -0200

    Removidas configurações por arquivo de propriedades

--- src/br/edu/fasatc/ec/fatbodygym/constansts/ErpDatabaseConstants.java
+++ src/br/edu/fasatc/ec/fatbodygym/constansts/ErpDatabaseConstants.java
@@ -2,12 +2,10 @@ package br.edu.fasatc.ec.fatbodygym.constansts;
 
 public class ErpDatabaseConstants {
 
-    public static final String EXTENSAO_TABELAS = Properties.Persistence.getTableExtension();
-
-    public static final String TABLE_USUARIOS = "table_usuarios.ser";
-    public static final String TABLE_ALUNOS = "table_alunos.ser";
-    public static final String TABLE_EXERCICIOS = "table_exercicios.ser";
-    public static final String TABLE_INSTRUTORES = "table_instrutores.ser";
-    public static final String TABLE_TREINOS = "table_treinos.ser";
+	public static final String TABLE_USUARIOS = "table_usuarios.ser";
+	public static final String TABLE_ALUNOS = "table_alunos.ser";
+	public static final String TABLE_EXERCICIOS = "table_exercicios.ser";
+	public static final String TABLE_INSTRUTORES = "table_instrutores.ser";
+	public static final String TABLE_TREINOS = "table_treinos.ser";
 
 }
